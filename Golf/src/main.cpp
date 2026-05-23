308049e99501fc740f0e5eb6a4a53ed42ed1a242
CheriseHeyl <cheriseheyl@gmail.com>
2026-05-23 12:19:41 +0200

    Fixed run issues, removed glad

--- Golf/src/main.cpp
+++ Golf/src/main.cpp
@@ -1,10 +1,10 @@
+#include <chrono>
+#include <iostream>
+#include <random>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <vector>
 #include <thread>
-#include <random>
-#include <chrono> 
+#include <vector>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -15,80 +15,96 @@
 using namespace glm;
 using namespace std;
 
-const char *getError()
-{
-    const char *errorDescription;
-    glfwGetError(&errorDescription);
-    return errorDescription;
+const char *getError() {
+  const char *errorDescription;
+  glfwGetError(&errorDescription);
+
+  if (errorDescription == nullptr)
+    return "Unknown GLFW error";
+
+  return errorDescription;
 }
 
-inline void startUpGLFW()
-{
-    glewExperimental = true; // Needed for core profile
-    if (!glfwInit())
-    {
-        throw getError();
-    }
+inline void startUpGLFW() {
+  glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
+
+  glewExperimental = true; // Needed for core profile
+  if (!glfwInit()) {
+    throw getError();
+  }
 }
 
-inline void startUpGLEW()
-{
-    glewExperimental = true; // Needed in core profile
-    if (glewInit() != GLEW_OK)
-    {
-        glfwTerminate();
-        throw getError();
-    }
+inline void startUpGLEW() {
+  glewExperimental = true; // Needed in core profile
+
+  GLenum err = glewInit();
+
+  // Clears harmless GLEW error caused by core profile
+  glGetError();
+
+  if (err != GLEW_OK) {
+    glfwTerminate();
+    throw (const char *)glewGetErrorString(err);
+  }
 }
 
-inline GLFWwindow *setUp()
-{
-    startUpGLFW();
-    glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // To make MacOS happy; should not be needed
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
-    GLFWwindow *window;                                            // (In the accompanying source code, this variable is global for simplicity)
-    window = glfwCreateWindow(1280, 720, "Team's Golf Course", NULL, NULL);
-    if (window == NULL)
-    {
-        cout << getError() << endl;
-        glfwTerminate();
-        throw "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n";
-    }
-    glfwMakeContextCurrent(window); // Initialize GLEW
-    startUpGLEW();
-    glEnable(GL_DEPTH_TEST);
-    glDepthFunc(GL_LESS);
-    return window;
-}
+inline GLFWwindow *setUp() {
+  startUpGLFW();
 
-int main()
-{
-    GLFWwindow *window;
-    try
-    {
-        window = setUp();
-    }
-    catch (const char *e)
-    {
-        cout << e << endl;
-        throw;
-    }
+  glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(window, true);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT,
+                 GL_TRUE); // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE,
+                 GLFW_OPENGL_CORE_PROFILE); // We don't want the old OpenGL
 
-        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  GLFWwindow *window; // (In the accompanying source code, this variable is
+                      // global for simplicity)
 
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+  window = glfwCreateWindow(1280, 720, "Team's Golf Course", NULL, NULL);
+
+  if (window == NULL) {
+    cout << getError() << endl;
 
     glfwTerminate();
-    return 0;
+
+    throw "Failed to open GLFW window. If you have an Intel GPU, they are not "
+          "3.3 compatible. Try the 2.1 version of the tutorials.\n";
+  }
+
+  glfwMakeContextCurrent(window); // Initialize GLEW
+
+  startUpGLEW();
+
+  glEnable(GL_DEPTH_TEST);
+  glDepthFunc(GL_LESS);
+
+  return window;
+}
+
+int main() {
+  GLFWwindow *window;
+
+  try {
+    window = setUp();
+  } catch (const char *e) {
+    cout << e << endl;
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window)) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+      glfwSetWindowShouldClose(window, true);
+
+    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
